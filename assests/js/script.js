6063187edc391b35819554eba407c2ade676d3ac
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-05-30 22:31:29 -0600

    Added fetch commands and put all data in to a weather object

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,47 +1,100 @@
 var mobileHistoryCounter = 0; 
-//if the length of the play history is over 2 set class d-sm-none, and if history is over 20 delete the 21st
-
+//if the length of the search history is over 2 set class d-sm-none, and if history is over 10 delete the 11th
+var weather = {};
 
 
 function getGeolocation() {
-    //gets a geolocation from the name of the city API_KEY: AIzaSyDcSaXUpjF8v5imMvCPScFZGSa0arEGi7o
-    var search = $("#city").val();
-    $("#city").val("");
-    search = search.trim();
-    search = search.split(" ")
-    var searchTemp = "CWC8%2BR9%20";
-    for (var i = 0; i < search.length; i++) {
-        searchTemp = search.join("%20" + search[i]);
-    }
-    search = searchTemp;
-
-    fetch("https://maps.googleapis.com/maps/api/geocode/json?adress=" + search + "key=IzaSyDcSaXUpjF8v5imMvCPScFZGSa0arEGi7o").then(function(responce) {
-
-    }).then(function(data) {
-
-    });
-}
-
-function getWeather() {
     //takes the city name and gets the weather values for it. It then sends it to the
-    
+    var search = $("#city").val();
+    search = search.trim();
+    var apiKey = "http://api.openweathermap.org/geo/1.0/direct?q=" + search +  
+    "&limit=1&appid=092da386c4cb34ff855fb9f8e6c59a3b";
 
-    fetch().then(function(responce) {
+    fetch(apiKey).then(function(responce) {
         //check to see if city comes up / if error in responce
-    }).then(function(data) {
-        //do stuff with the data
+        if (responce.ok) {
+            responce.json().then(function(data) {
+                //do stuff with the data
+                var lat = data[0].lat;
+                var lon = data[0].lon;
+                getWeather(lat, lon)
+            });
+        }
+        else {
+            alert("City Not found or server is down. Please try again.");
+        }
+        
     });
 
 }
 
-function searchClicked() {
+function getWeather(lat, lon) {
+    weather = {};
+    var apiKey = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon +  
+    "&units=imperial&exclude=minutely,hourly,alerts&appid=092da386c4cb34ff855fb9f8e6c59a3b";
+
+    fetch(apiKey).then(function(responce) {
+        //check to see if city comes up / if error in responce
+        if (responce.ok) {
+            responce.json().then(function(data) {
+                //do stuff with the data
+                var date = [];
+                var temp = [];
+                var tempLow = [];
+                var wind = [];
+                var humidity = [];
+                var uvIndex = null;
+                var icon = [];
+                var current = true;
+
+                for (var i = 0; i < 6; i++) {
+                    if (current) {
+                        current = false;
+                        date.push(moment().format("MM,DD,YYYY"));
+                        temp.push(data.current.temp);
+                        wind.push(data.current.wind_speed);
+                        humidity.push(data.current.humidity);
+                        uvIndex = data.current.uvi;
+                        icon.push(data.current.weather[0].main);
+                        i--;
+                    }
+                    else {
+                        date.push(moment().format("MM,DD,YYYY"));
+                        temp.push(data.daily[i].temp.max);
+                        tempLow.push(data.daily[i].temp.min)
+                        wind.push(data.daily[i].wind_speed);
+                        humidity.push(data.daily[i].humidity);
+                        icon.push(data.daily[i].weather[0].main);
+                    }
+                }
+
+                weather = {
+                    date: date,
+                    temp: temp,
+                    wind: wind,
+                    humidity: humidity,
+                    uvIndex: uvIndex,
+                    icon: icon
+                };
+
+            });
+        }
+        else {
+            alert("ERROR: Unkown Issue");
+        }
+        
+    });
+}
+
+function searchClicked(event) {
      //Starts up funcitons for displaying weather for current city
+     event.preventDefault();
      var check = $("#city").val();
      if (check) {
          getGeolocation();
      }
      else {
-         alert("Please enter a city name to search!");
+         alert("Please enter a city name!")
      }
 }
 
@@ -61,4 +114,6 @@ function loadHistory() {
 
 //Inital functions and event listeners
 
-$("#city-form").on("submit", searchClicked());
+$("#city-form").on("submit", searchClicked);
+
+
