06688b1f252998a96937e3f4b53ea36a048e4fc1
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-05-31 18:01:12 -0600

    added getIcon class and top variables, added displayWeather function

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,6 +1,23 @@
 var mobileHistoryCounter = 0; 
 //if the length of the search history is over 2 set class d-sm-none, and if history is over 10 delete the 11th
-var weather = {};
+var iconWeatherClasses = ["bi-cloud-lightning-rain", "bi-cloud-lightning", "bi-lightning", "bi-cloud-drizzle", 
+"bi-cloud-rain", "bi-cloud-rain-heavy", "bi-cloud-snow", "bi-cloud-sleet", "bi-cloud-fog", "bi-cloud-fog2", "bi-cloud-haze", 
+"bi-cloud-haze-1", "bi-cloud-haze-fill", "bi-tornado", "bi-brightness-high", "bi-cloud-sun", "bi-cloud", "bi-clouds"]
+var iconWeatherIndex = {
+    200: "bi-cloud-lightning-rain",201: "bi-cloud-lightning-rain",202: "bi-cloud-lightning-rain",
+    210: "bi-cloud-lightning",211: "bi-cloud-lightning",212: "bi-cloud-lightning",221: "bi-lightning",
+    230: "bi-cloud-lightning-rain",231: "bi-cloud-lightning-rain",232: "bi-cloud-lightning-rain",
+    300: "bi-cloud-drizzle",301: "bi-cloud-drizzle",302: "bi-cloud-drizzle",310: "bi-cloud-drizzle",
+    311: "bi-cloud-drizzle",312: "bi-cloud-drizzle",313: "bi-cloud-rain",314: "bi-cloud-rain",
+    321: "bi-cloud-rain",500: "bi-cloud-rain",501: "bi-cloud-rain",502: "bi-cloud-rain",503: "bi-cloud-rain-heavy",
+    504: "bi-cloud-rain-heavy",511: "bi-snow",520: "bi-cloud-rain-",521: "bi-cloud-rain-",522: "bi-cloud-rain-heavy",
+    531: "bi-cloud-rain-heavy",600: "bi-cloud-snow",601: "bi-cloud-snow",602: "bi-cloud-snow",611: "bi-cloud-sleet",
+    612: "bi-cloud-sleet",613: "bi-cloud-sleet",615: "bi-cloud-sleet",616: "bi-cloud-sleet", 620: "bi-cloud-snow",
+    621: "bi-cloud-snow",622: "bi-cloud-snow",701: "bi-cloud-fog2",711: "bi-cloud-haze-1",721: "bi-cloud-haze",
+    731: "bi-cloud-haze-fill",741: "bi-cloud-fog",751: "bi-cloud-haze-fill",761: "bi-cloud-haze-fill",
+    762: "bi-cloud-haze-fill",771: "bi-cloud-haze-fill",781: "bi-tornado",800: "bi-brightness-high",
+    801: "bi-cloud-sun",802: "bi-cloud",803: "bi-clouds",804: "bi-clouds",
+}
 
 
 function getGeolocation() {
@@ -17,7 +34,7 @@ function getGeolocation() {
                 //do stuff with the data
                 var lat = data[0].lat;
                 var lon = data[0].lon;
-                getWeather(lat, lon)
+                getWeather(lat, lon, search);
             });
         }
         else {
@@ -28,8 +45,8 @@ function getGeolocation() {
 
 }
 
-function getWeather(lat, lon) {
-    weather = {};
+function getWeather(lat, lon, city) {
+    var weather = {};
     var apiKey = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon +  
     "&units=imperial&exclude=minutely,hourly,alerts&appid=092da386c4cb34ff855fb9f8e6c59a3b";
 
@@ -55,16 +72,16 @@ function getWeather(lat, lon) {
                         wind.push(data.current.wind_speed);
                         humidity.push(data.current.humidity);
                         uvIndex = data.current.uvi;
-                        icon.push(data.current.weather[0].main);
+                        icon.push(data.current.weather[0].id);
                         i--;
                     }
                     else {
-                        date.push(moment().format("MM,DD,YYYY"));
+                        date.push(moment().format("MM,DD,YYYY").add((i + 1), "days"));
                         temp.push(data.daily[i].temp.max);
                         tempLow.push(data.daily[i].temp.min)
                         wind.push(data.daily[i].wind_speed);
                         humidity.push(data.daily[i].humidity);
-                        icon.push(data.daily[i].weather[0].main);
+                        icon.push(data.daily[i].weather[0].id);
                     }
                 }
 
@@ -74,9 +91,10 @@ function getWeather(lat, lon) {
                     wind: wind,
                     humidity: humidity,
                     uvIndex: uvIndex,
-                    icon: icon
+                    icon: icon,
+                    city: city
                 };
-
+                displayWeather(weather);
             });
         }
         else {
@@ -98,8 +116,46 @@ function searchClicked(event) {
      }
 }
 
-function getIcon() {
+function getIcon(weather, index) {
     //Based of the weather data, sets a value for the weather icon to be displayed
+    $("#icon-" + index).removeClass(iconWeatherClasses);
+    if (index === 0) {
+        var baseClasses = "icon-lg bi ";
+    }
+    else {
+        var baseClasses = "icon-sm bi ";
+    }
+    for (var i = 0; i < iconWeatherClasses.length; i++) {
+        if ($("#icon-" + index).hasClass(iconWeatherClasses[i])) {
+            $("#icon-" + index).removeClass(iconWeatherClasses[i]);
+        }
+    }
+    var classIndex = weather.icon;
+    $("icon-" + index).addClass(iconWeatherIndex.classIndex);
+    
+
+
+    
+}
+
+function displayWeather(weather) {
+    //updates html with new weather data
+    if ($("#display-weather").hasClass("d-none")) {
+        $("#display-weather").removeClass("d-none");
+    }
+
+    $("#uv-index").text(weather.uvIndex);
+    $("#city-displayed").text(weather.city + ":");
+
+    for (var i = 0; i < weather.tempLow.length; i++) {
+        $("#temp-" + i + "-low").text(weather.tempLow[i]);
+    }
+    for (var i = 0; i < weather.temp.length; i++) {
+        $("#temp-" + i).text(weather.temp[i]);
+        $("#wind-" + i).text(weather.wind[i]);
+        $("#hum-" + i).text(weather.humidity[i]);
+        $("#icon-" + i).addClass(getIcon(weather, i));
+    }
 }
 
 function saveHistory(){
